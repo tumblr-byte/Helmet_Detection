c4c203a035c2ba7753c469167e650bffca5dde0c
Pooja Vachhad <likeway64@gmail.com>
2026-09-08 12:37:31 +0530

    Update main.py

--- Helmet-Detection/main.py
+++ Helmet-Detection/main.py
@@ -41,7 +41,7 @@ int_to_char = {idx: char for char, idx in char_to_int.items()}
 # Image transforms for CRNN
 test_transforms = A.Compose([
     A.Resize(100 , 200),
-    A.Normalize(mean = (0.485 , 0.456 , 0.406) , std = (0.229 , 0.224 , 0.224)),
+    A.Normalize(mean = (0.485 , 0.456 , 0.406) , std = (0.229 , 0.224 , 0.225)),
     ToTensorV2(),
 
 ])
